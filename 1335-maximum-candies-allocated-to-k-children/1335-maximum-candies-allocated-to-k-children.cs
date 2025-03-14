54ce7bdcd7760638feded70ef16eee332b56db91
이호윤 <68680118+HaTToek@users.noreply.github.com>
2025-03-14 23:20:49 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int MaximumCandies(int[] candies, long k) {
+        int total = candies.Sum();
+        if(total < k) return 0;
+
+        int left = 1, right = candies.Max();
+        int result = 0;
+
+        while(left <= right){
+            int mid = (left + right) / 2;
+            long childCount = 0;
+
+            foreach(int candy in candies){
+                childCount += candy / mid;
+            }
+
+            if(childCount >= k){
+                result = mid;
+                left = mid + 1;
+            }
+            else{
+                right = mid -1;
+            }
+        }
+        return result;
+    }
+}
