32b92fccd4fbc1544a76c8c8731356f89fd605d4
이호윤 <68680118+HaTToek@users.noreply.github.com>
2025-03-19 11:14:39 +0900

    Time: 8 ms (40%), Space: 62.4 MB (5%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cs
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.cs
@@ -1,14 +1,15 @@
 public class Solution {
     public int MinOperations(int[] nums) {
-        int min = 0;
-        for(int i=0;i<nums.Count()-2;i++){
-            if(nums[i] == 0){
-                for(int j=0;j<3;j++){
-                    nums[i+j] = nums[i+j] == 0 ? 1 : 0;
-                }
-                min++;
+        int min = 0, count = nums.Count();
+        for(int i=0;i<count-2;i++){
+            if(nums[i] != 0) {
+                continue;
             }
+            nums[i] ^= 1;
+            nums[i+1] ^= 1;
+            nums[i+2] ^= 1;
+            min++;
         }
-        return nums.Count(a => a == 0) == 0 ? min : -1;
+        return (nums[count-1] & nums[count-2]) != 1 ? -1 : min;
     }
 }
