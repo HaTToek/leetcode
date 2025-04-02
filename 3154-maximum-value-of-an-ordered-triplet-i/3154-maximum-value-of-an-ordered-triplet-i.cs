5af1ab0f3acad2da0bc992a41017a522693fd2c7
이호윤 <68680118+HaTToek@users.noreply.github.com>
2025-04-02 23:34:08 +0900

    Time: 5 ms (7.41%), Space: 43.6 MB (9.26%) - LeetHub

--- 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cs
+++ 3154-maximum-value-of-an-ordered-triplet-i/3154-maximum-value-of-an-ordered-triplet-i.cs
@@ -3,6 +3,7 @@ public class Solution {
         long max = 0;
         for(int i=0;i<nums.Length-2;i++){
             for(int j=i+1;j<nums.Length-1;j++){
+                if(nums[i] < nums[j]) continue;
                 for(int k=j+1;k<nums.Length;k++){
                     max = Math.Max(max, (long)(nums[i] - nums[j]) * nums[k]);
                 }   
